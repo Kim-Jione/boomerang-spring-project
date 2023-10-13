69bf1203d830f59922aa1bc09c489996dbbf12c8
SungUnJang <skdnj48@gmail.com>
2023-10-13 16:55:22 +0900

    fix]frontEnd 임시 통합

--- js/main.js
+++ js/main.js
@@ -1,4 +1,25 @@
-
+// 반응형 슬라이드
+const swiperEl = document.querySelector('.Hot_Swiper')
+Object.assign(swiperEl, {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 5,
+      spaceBetween: 50,
+    },
+  },
+});
+swiperEl.initialize();
 
 
 
