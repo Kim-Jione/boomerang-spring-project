d3e137f0daf77559a317aef3bf3e3f6e8a14ea0e
Jiwon-Kim <hghg1036@naver.com>
2023-10-18 11:47:22 +0900

    View: 관리자 템플릿 프로젝트 적용

---
+++ src/main/java/com/example/bumerang/web/AnminController.java
@@ -0,0 +1,34 @@
+package com.example.bumerang.web;
+
+import com.example.bumerang.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpSession;
+
+
+@RequiredArgsConstructor
+@Controller
+public class AnminController {
+    private final HttpSession session;
+    private final UserService userService;
+
+    // 메인 화면
+    @GetMapping("/indexForm")
+    public String indexForm() {
+        return "admin/indexForm";
+    }
+
+    // 차트 화면
+    @GetMapping("/chartForm")
+    public String chartForm() {
+        return "admin/chartForm";
+    }
+
+    // 테이블 화면
+    @GetMapping("/tableForm")
+    public String tableForm() {
+        return "admin/tableForm";
+    }
+}
