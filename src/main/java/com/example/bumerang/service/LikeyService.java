a636a9cc3a4baaf147994921f939f13249aff5b0
Jiwon-Kim <hghg1036@naver.com>
2023-10-25 10:04:27 +0900

    Feat: 추천 기능 구현

--- src/main/java/com/example/bumerang/service/LikeyService.java
+++ src/main/java/com/example/bumerang/service/LikeyService.java
@@ -1,5 +1,6 @@
 package com.example.bumerang.service;
 
+import com.example.bumerang.domain.likey.Likey;
 import com.example.bumerang.domain.likey.LikeyDao;
 import com.example.bumerang.web.dto.request.likey.LikeyDto;
 import lombok.RequiredArgsConstructor;
@@ -12,9 +13,56 @@ public class LikeyService {
 
 	private final LikeyDao likeyDao;
 
-
-	public void likey(Integer userId, LikeyDto likeyDto) {
-		likeyDto.setUserId(userId);
-		likeyDao.insert(likeyDto.toLikey());
+	public Integer findByJobId(Integer userId, Integer jobId) {
+		Integer findByJobId = likeyDao.findByJobId(userId, jobId);
+		return findByJobId;
 	}
+
+	public Integer findByPfId(Integer userId, Integer pfId) {
+		Integer findByPfId = likeyDao.findByPfId(userId, pfId);
+		return findByPfId;
+	}
+
+	public Integer findByCommentId(Integer userId, Integer commentId) {
+		Integer findByCommentId = likeyDao.findByCommentId(userId, commentId);
+		return findByCommentId;
+	}
+
+	public Likey likeyJob(LikeyDto likeyDto) {
+		likeyDao.insert(likeyDto.toLikey());
+		Likey likeyJob = likeyDao.findByRecent();
+		return likeyJob;
+	}
+
+	public Likey likeyPf(LikeyDto likeyDto) {
+		likeyDao.insert(likeyDto.toLikey());
+		Likey likeyJob = likeyDao.findByRecent();
+		return likeyJob;
+	}
+
+	public Likey likeyComment(LikeyDto likeyDto) {
+		likeyDao.insert(likeyDto.toLikey());
+		Likey likeyComment = likeyDao.findByRecent();
+		return likeyComment;
+	}
+
+	public Likey unLikeyJob(Integer jobLikeyId) {
+		Likey deleteResult = likeyDao.findById(jobLikeyId);
+		likeyDao.delete(jobLikeyId);
+		return deleteResult;
+	}
+
+	public Likey unLikeyPf(Integer pfLikeyId) {
+		Likey deleteResult = likeyDao.findById(pfLikeyId);
+		likeyDao.delete(pfLikeyId);
+		return deleteResult;
+	}
+
+
+	public Likey unLikeyComment(Integer commentLikeyId) {
+		Likey deleteResult = likeyDao.findById(commentLikeyId);
+		likeyDao.delete(commentLikeyId);
+		return deleteResult;
+	}
+
 }
--- src/main/java/com/example/bumerang/web/LikeyController.java
+++ src/main/java/com/example/bumerang/web/LikeyController.java
@@ -8,6 +8,7 @@ import com.example.bumerang.web.dto.response.CMRespDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpSession;
@@ -20,61 +21,52 @@ public class LikeyController {
     private final HttpSession session;
     private final LikeyService likeyService;
 
-    // 구인글 좋아요 기능
-//    @PostMapping("/likey/{jobId}")
-//    public @ResponseBody CMRespDto<?> likey(LikeyDto likeyDto) {
-//        SessionUserDto principal = (SessionUserDto) session.getAttribute("principal");
-//        if (principal == null) {
-//            return new CMRespDto<>(-1, "로그인을 진행해주세요.", null);
-//        }
-//        Integer jobId = likeyDto.getJobId();
-//        Integer pfId = likeyDto.getPfId();
-//        Integer userId = principal.getUserId();
-//        Integer jobLikeyId = likeyService.findByJobId(userId, jobId);
-//        Integer pfLikeyId = likeyService.findByPfId(userId, pfId);
-//
-//
-//        if (jobLikeyId == null) {
-//            Likey likeyPS = likeyService.likey(principal.getUserId(), jobId);
-//            return new CMRespDto<>(1, "구인글 추천 성공", likeyPS);
-//        }
-//
-//        Likey likeyPS = likeyService.unLikey(likeyId);
-//        return new CMRespDto<>(1, "구인글 추천 취소 성공", likeyPS);
-//    }
-
-    // 구인글 좋아요 기능
-    @PostMapping("/likey/{jobId}")
-    public @ResponseBody CMRespDto<?> likey(LikeyDto likeyDto) {
+    // 추천 기능
+    @PostMapping("/likey")
+    public @ResponseBody CMRespDto<?> likey(@RequestBody LikeyDto likeyDto) {
         SessionUserDto principal = (SessionUserDto) session.getAttribute("principal");
         if (principal == null) {
             return new CMRespDto<>(-1, "로그인을 진행해주세요.", null);
         }
         Integer jobId = likeyDto.getJobId();
         Integer pfId = likeyDto.getPfId();
-        Integer userId = principal.getUserId();
+        Integer commentId = likeyDto.getCommentId();
+        likeyDto.setUserId(principal.getUserId());
+        Integer userId = likeyDto.getUserId();
         Integer jobLikeyId = likeyService.findByJobId(userId, jobId);
         Integer pfLikeyId = likeyService.findByPfId(userId, pfId);
+        Integer commentLikeyId = likeyService.findByCommentId(userId, commentId);
 
-        // 구인글 좋아요
+        // 구인글 추천
         if (jobId != null) {
             if (jobLikeyId == null) {
-                Likey jobLikey = likeyService.likeyJob(userId, jobId);
+                Likey jobLikey = likeyService.likeyJob(likeyDto);
                 return new CMRespDto<>(1, "구인글 추천 성공", jobLikey);
             }
-            Likey jobLikey = likeyService.unLikey(jobLikeyId);
+            Likey jobLikey = likeyService.unLikeyJob(jobLikeyId);
             return new CMRespDto<>(1, "구인글 추천 취소 성공", jobLikey);
         }
 
-        // 공연글 좋아요
+        // 공연글 추천
         if (pfId != null) {
             if (pfLikeyId == null) {
-                Likey pfLikey = likeyService.likeyPf(userId, pfId);
+                Likey pfLikey = likeyService.likeyPf(likeyDto);
                 return new CMRespDto<>(1, "공연글 추천 성공", pfLikey);
             }
-            Likey pfLikey = likeyService.unLikey(pfLikeyId);
+            Likey pfLikey = likeyService.unLikeyPf(pfLikeyId);
             return new CMRespDto<>(1, "공연글 추천 취소 성공", pfLikey);
         }
 
+        // 공연글 추천
+        if (commentId != null) {
+            if (commentLikeyId == null) {
+                Likey commentLikey = likeyService.likeyComment(likeyDto);
+                return new CMRespDto<>(1, "댓글 추천 성공", commentLikey);
+            }
+            Likey commentLikey = likeyService.unLikeyComment(commentLikeyId);
+            return new CMRespDto<>(1, "댓글 추천 취소 성공", commentLikey);
+        }
+
+        return new CMRespDto<>(-1, "데이터 요청을 다시 해주세요.", null);
     }
 }
