9893daff94c24378c87597826f911c007318645c
Jiwon-Kim <hghg1036@naver.com>
2023-11-04 11:52:13 +0900

    View: 404 예시 화면 구현

--- src/main/java/com/example/bumerang/web/UserController.java
+++ src/main/java/com/example/bumerang/web/UserController.java
@@ -64,8 +64,8 @@ public class UserController {
     }
     // 로그인 화면
     @GetMapping("/user/loginForm")
-    public String loginForm() {
-        return "loginForm";
+    public @ResponseBody CMRespDto<?> loginForm() {
+        return new CMRespDto<>(1, "로그인 화면 불러오기 성공.", null);
     }
 
     // 로그인 기능
