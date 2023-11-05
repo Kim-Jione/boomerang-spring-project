a3b5377d0bad39b8a26c69d0d43b53458845c170
kim_jiwon <106166409+Kim-Jione@users.noreply.github.com>
2023-11-05 16:24:47 +0900

    Merge: 헤더 페이지 연결 (#76)
    - 공연글 쓰기 페이지 이외에 페이지 이동 연결
    - 로그인, 회원가입 기능 연결

--- src/main/java/com/example/bumerang/web/NoticeController.java
+++ src/main/java/com/example/bumerang/web/NoticeController.java
@@ -1,40 +1,48 @@
 package com.example.bumerang.web;
 
-import com.example.bumerang.domain.comment.Comment;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 import com.example.bumerang.domain.notice.Notice;
 import com.example.bumerang.service.NoticeService;
-import com.example.bumerang.web.dto.SessionUserDto;
-import com.example.bumerang.web.dto.request.comment.CommentDto;
-import com.example.bumerang.web.dto.request.notice.WriteDto;
-import com.example.bumerang.web.dto.response.CMRespDto;
 import com.example.bumerang.web.dto.response.notice.DetailFormDto;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpSession;
-import java.util.List;
+import lombok.RequiredArgsConstructor;
 
 
 @RequiredArgsConstructor
 @Controller
 public class NoticeController {
 
-    private final HttpSession session;
     private final NoticeService noticeService;
 
     // 공지사항 목록 화면
     @GetMapping("/notice/listForm")
-    public @ResponseBody CMRespDto<?> listForm() {
+    public String listForm(Model model) {
         List<Notice> noticeList = noticeService.findAll();
-        return new CMRespDto<>(1, "공지사항 목록 화면 불러오기 성공.", noticeList);
+        model.addAttribute("noticeList", noticeList);
+        return "noticeListForm";
     }
 
     // 공지사항 상세보기 화면
     @GetMapping("/notice/detailForm/{noticeId}")
-    public @ResponseBody CMRespDto<?> detailForm(@PathVariable Integer noticeId) {
+    public String detailForm(@PathVariable Integer noticeId,Model model) {
         DetailFormDto noticeDetail = noticeService.findByNotice(noticeId);
-        return new CMRespDto<>(1, "공지사항 상세보기 화면 불러오기 성공.", noticeDetail);
+        if(noticeDetail==null){
+            return "redirect:/404";
+        }
+        model.addAttribute("notice", noticeDetail);
+        return "noticeDetailForm";
+    }
+
+    // 공지글 작성하기 화면
+    @GetMapping("/s/api/auth/notice/writeForm")
+    public String writeForm() {
+        return "noticeWriteForm";
     }
 
 }
