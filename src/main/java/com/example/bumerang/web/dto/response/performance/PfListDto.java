d951d76ad43c6ea5f1f409b8de67ac8ad78a71b2
getbravelee <getbravelee@gmail.com>
2023-11-07 22:53:08 +0900

    View: 공연 메인 뷰

--- src/main/java/com/example/bumerang/web/dto/response/performance/PfListDto.java
+++ src/main/java/com/example/bumerang/web/dto/response/performance/PfListDto.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 @Getter
 @Setter
 public class PfListDto {
+    private Integer pfId;
     private String pfTitle;
     private String pfLocation;
     private String pfStartDate;
@@ -13,6 +14,9 @@ public class PfListDto {
     private String pfThumbnail;
     private Integer viewCount;
     private String pfAgerating;
+    private String pfGenre;
     // DB 칼럼 아님
     private Boolean isPrice; // true = 유료 false = 무료
+    private Boolean isDead; // 마감됐는지 확인 여부 true = 상영중 false = 마감
+    private String imgPath;
 }
