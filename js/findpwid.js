786370f5f3dca659eac213f9a109e238228771d7
wjdtjddjq123 <59285584+wjdtjddjq123@users.noreply.github.com>
2023-11-07 11:18:17 +0900

    Fix : 비밀번호찾기, 아이디찾기 기능 수정
    Fix : 비밀번호찾기, 아이디찾기 기능 수정

--- js/findpwid.js
+++ js/findpwid.js
@@ -1,21 +1,31 @@
 function init() {
     var pwBtn = document.querySelector('.pw_button');
     var idBtn = document.querySelector('.id_button');
-    var pwBox = document.querySelector('.pwbox');
-    var idBox = document.querySelector('.idbox');
-    var emailBtn = document.querySelector('.emaillink');
-  
+
+    var pwonoffBox = document.querySelector('.pwonoffBox');
+    var idonoffBox = document.querySelector('.idbox');
+
+    var pwEmailBtn = document.getElementById('pwemailBtn');
+    var idEmailBtn = document.getElementById('idemailBtn');
+
+    var pw_button_box = document.querySelector('.pw_button_box');
+    var id_button_box = document.querySelector('.id_button_box');
+   
     pwBtn.addEventListener('click', function() {
-      pwBox.style.visibility = 'visible';
-      idBox.style.visibility = 'hidden';
+        pwonoffBox.style.display = 'flex';
+        pw_button_box.style.display ='flex';
+        idonoffBox.style.display = 'none';
+        id_button_box.style.display ='none';
     });
   
     idBtn.addEventListener('click', function() {
-      idBox.style.visibility = 'visible';
-      pwBox.style.visibility = 'hidden';
+        idonoffBox.style.display = 'flex';
+        id_button_box.style.display ='flex';
+        pw_button_box.style.display ='none';
+        pwonoffBox.style.display = 'none';
     });
 
-    emailBtn.addEventListener('click', function(){
+    pwEmailBtn.addEventListener('click', function(){
         const emailInput = document.getElementById("emailInput"); // 수정: getElementById 사용
         const email = emailInput.value;
 
@@ -25,6 +35,22 @@ function init() {
             // 이메일 형식이 올바른 경우
             alert('이메일이 발송되었습니다.');
             emailInput.value = ""; // 입력 필드 초기화
+        } else {
+            // 이메일 형식이 잘못된 경우
+            alert('이메일 형식이 올바르지 않습니다. 다시 시도해주세요.');
+        }
+    });
+
+    idEmailBtn.addEventListener('click', function(){
+        const idInput = document.getElementById("idInput"); // 수정: getElementById 사용
+        const id = idInput.value;
+
+        const pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        console.log(id);
+        if (pattern.test(id)) {
+            // 이메일 형식이 올바른 경우
+            alert('이메일이 발송되었습니다.');
+            idlInput.value = ""; // 입력 필드 초기화
         } else {
             // 이메일 형식이 잘못된 경우
             alert('이메일 형식이 올바르지 않습니다. 다시 시도해주세요.');
