a03744aad85acfbfc45a25458f92c3da75285799
KimHugo <hugobayit@gmail.com>
2023-11-08 14:57:18 +0900

    Fix]네비 토글 외 영역 클릭시 네비 토글 닫힘

--- js/default.js
+++ js/default.js
@@ -1,10 +1,30 @@
-function menuToggle(){
-    const toggleMenu = document.querySelector('.menu');
-    toggleMenu.classList.toggle('active')
+const toggleMenu = document.querySelector(".menu");
+const toggleMenu2 = document.querySelector(".menu2");
+
+function menuToggle() {
+  //   const toggleMenu = document.querySelector(".menu");
+  toggleMenu.classList.toggle("active");
 }
 
-function menu2Toggle(){
-    const toggleMenu = document.querySelector('.menu2');
-    toggleMenu.classList.toggle('active')
+function menu2Toggle() {
+  //   const toggleMenu = document.querySelector(".menu2");
+  toggleMenu2.classList.toggle("active");
 }
 
+function isClickToggle(event) {
+  if (!toggleMenu.parentElement.contains(event.target)) {
+    if (toggleMenu.classList.contains("active")) {
+      toggleMenu.classList.toggle("active");
+    }
+  }
+
+  if (!toggleMenu2.parentElement.contains(event.target)) {
+    if (toggleMenu2.classList.contains("active")) {
+      toggleMenu2.classList.toggle("active");
+    }
+  }
+}
+
+document.addEventListener("click", function (event) {
+  isClickToggle(event);
+});
